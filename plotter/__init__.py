2b9de232ff8526bc0a2ff843316c587c4dd7ce95
lmarchetti02 <00lucamarchetti00@gmail.com>
2024-01-27 01:00:29 +0100

    initial commit

---
+++ plotter/__init__.py
@@ -0,0 +1,22 @@
+import os
+
+# create necessary directories
+try:
+    os.mkdir("./img")
+except FileExistsError as _:
+    print("The '/img' directory already exists.")
+
+try:
+    os.mkdir("./log")
+except FileExistsError as _:
+    print("The '/log' directory already exists.")
+
+try:
+    os.mkdir("./text")
+except FileExistsError as _:
+    print("The '/text' directory already exists.")
+
+try:
+    os.mkdir("./info")
+except FileExistsError as _:
+    print("The '/text' directory already exists.")
