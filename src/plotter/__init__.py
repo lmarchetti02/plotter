072b8090569054c25f40a2d1318a83bae41c7acd
lmarchetti02 <00lucamarchetti00@gmail.com>
2024-01-28 01:07:03 +0100

    implemented copying of data-files to info dir

--- src/plotter/__init__.py
+++ src/plotter/__init__.py
@@ -1,23 +1,24 @@
 import os
-import importlib
+import importlib.metadata as im
 
 # create necessary directories
-try:
-    os.mkdir("./img")
-except FileExistsError as _:
-    print("The '/img' directory already exists.")
+parent_dir = "./plotter"
+os.mkdir(parent_dir)
 
-try:
-    os.mkdir("./log")
-except FileExistsError as _:
-    print("The '/log' directory already exists.")
+dirs = ("/img", "/log", "/text", "/info")
+for d in dirs:
+    try:
+        os.mkdir((destination := parent_dir + d))
+    except OSError as _:
+        print(f"The {destination} directory already exists.")
 
-try:
-    os.mkdir("./text")
-except FileExistsError as _:
-    print("The '/text' directory already exists.")
+# copy data-files to info/
+package_name = "plotter"
+data_dir = "utils"
+destination = os.getcwd() + "/plotter/info/"
 
-try:
-    os.mkdir("./info")
-except FileExistsError as _:
-    print("The '/text' directory already exists.")
+files = [f for f in im.files(package_name) if "utils/" in str(f)]  # find all files
+for f in files:
+    f_destination = destination + f.name
+    with open(f_destination, "wb") as d_file:
+        d_file.write(f.read_binary())
