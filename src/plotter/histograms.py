61fb4bc60c786b8e9c86f482262ce72b3d607e5d
lmarchetti02 <00lucamarchetti00@gmail.com>
2024-02-10 11:44:25 +0100

    started implementing Hist2D class

--- src/plotter/histograms.py
+++ src/plotter/histograms.py
@@ -6,7 +6,6 @@ import numpy as np
 logger = logging.getLogger(__name__)
 
 
-# TODO: add documentation
 class Hist:
     """
     Class used for creating a 1D histogram,
@@ -42,8 +41,6 @@ class Hist:
         self.__density = density
         self.__cumulative = cumulative
 
-        self.__bin_vals_counter = 0
-        self.__bins_counter = 0
         self.bin_vals = None
         self.bins = None
 
@@ -54,9 +51,6 @@ class Hist:
 
         The data member `bin_vals` is an array containing
         the values corresponding to each bin of the histogram.
-
-        It is possible to obtain it for further analysis,
-        but not to modify its value.
         """
 
         logger.info("Called 'Hist.bin_vals' getter")
@@ -67,16 +61,9 @@ class Hist:
     def bin_vals(self, value):
         logger.info("Called 'Hist.bin_vals' setter")
 
-        if self.__bin_vals_counter < 2:
-            logger.debug("First call => modified 'bin_vals'")
+        # TODO: add checks
 
-            self._bin_vals = value
-            self.__bin_vals_counter += 1
-            return
-
-        logger.warning("User tried to modify 'Hist.bin_vals'")
-
-        pass
+        self._bin_vals = value
 
     @property
     def bins(self):
@@ -86,9 +73,6 @@ class Hist:
         The data member `bins` is an array containing the
         edges of the bins. Therefore, its size is equal
         to the number of bins +1.
-
-        It is possible to obtain it for further analysis,
-        but not to modify its value.
         """
 
         logger.info("Called 'Hist.bins' getter")
@@ -99,16 +83,106 @@ class Hist:
     def bins(self, value):
         logger.info("Called 'Hist.bins' setter")
 
-        if self.__bins_counter < 2:
-            logger.debug("First call => modified 'bins'")
+        logger.debug("First call => modified 'bins'")
 
-            self._bins = value
-            self.__bins_counter += 1
-            return
+        self._bins = value
 
-        logger.warning("User tried to modify 'Hist.bins'")
+    def draw(
+        self,
+        canvas: Canvas,
+        range: Optional[tuple] = None,
+        color: Optional[str] = "cornflowerblue",
+        alpha: Optional[float] = 1,
+    ) -> None:
+        """
+        This function draws the histogram in the canvas
+        to which it belongs.
 
-        pass
+        Parameters
+        ---
+        canvas: Canvas
+            The canvas object to which the histogram
+            is to be attached.
+
+        Optional Parameters
+        ---
+        range: tuple
+            The tuple with the left and right limits
+            of the bins. It is set to `None` by default.
+        color: str
+            The matplotlib color of the histogram.
+        alpha: float
+            The transparency of the histogram.
+        """
+
+        logger.info("Called 'Hist.draw()'")
+
+        canvas.counter_histograms += 1
+
+        self.bin_vals, self.bins, _ = canvas.ax.hist(
+            self.__data,
+            bins=self.__nbins,
+            range=range,
+            density=self.__density,
+            cumulative=self.__cumulative,
+            histtype="stepfilled",
+            color=color,
+            alpha=alpha,
+            label=canvas.text.histograms[canvas.counter_histograms - 1],
+        )
+
+        logger.debug(f"Hist {canvas.counter_histograms-1} drawn")
+
+
+class Hist2D:
+    """
+    Class used for creating a 1D histogram,
+    which is then drawn in a canvas.
+
+    Parameters
+    ---
+    data: numpy.ndarray
+        The array containing the data to plot.
+    nbins: int
+        The number of bins of the histogram.
+        See matplotlib documentation. It is set to
+        `"auto"` by default.
+    density: bool
+        Whether to normalize the histogram. It is
+        set to `False` by default.
+    cumulative: bool
+        Whether to plot the cumulative histogram
+        or not. It is set to `False` by default.
+    """
+
+    def __init__(
+        self,
+        x: np.ndarray,
+        y: np.ndarray,
+        density: Optional[bool] = False,
+        **kwargs,  # bins
+    ) -> None:
+        logger.info("Created 'Hist2D' object")
+
+        self.__x = x
+        self.__y = y
+        self.__density = density
+
+        self.bin_vals = None
+        self.xbins = None
+        self.ybins = None
+
+    @property
+    def bin_vals(self):
+        return self._bin_vals
+
+    @property
+    def xbins(self):
+        return self._xbins
+
+    @property
+    def ybins(self):
+        return self._ybins
 
     def draw(
         self,
