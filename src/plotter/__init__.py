622c73b1a29125329c88b36af0c5fbac72280336
Luca Marchetti <90146345+lmarchetti02@users.noreply.github.com>
2024-10-11 18:54:13 +0200

    minor changes

--- src/plotter/__init__.py
+++ src/plotter/__init__.py
@@ -1,5 +1,10 @@
 """
+-----------------------------------------------------
+PLOTTER 
+by Luca Marchetti
+
 A small Python library for plotting beautiful graphs.
+------------------------------------------------------
 """
 
 import os
@@ -7,18 +12,19 @@ import pathlib
 import importlib.metadata as im
 
 # create necessary directories
-parent_dir = "./plotter"
+parent_dir = pathlib.Path("./plotter")
 try:
+    # make directories
     os.mkdir(parent_dir)
 
-    dirs = ("/img", "/log", "/text", "/utils", "/utils/info")
-    for d in dirs:
+    dirs = ("img", "log", "text", "utils", "utils/info")
+    for dir in dirs:
         try:
-            os.mkdir((destination := parent_dir + d))
+            os.mkdir((destination := parent_dir.joinpath(dir)))
         except OSError as _:
             print(f"{destination} directory already exists.")
 
-    # copy data-files to utils/ and utils/info/
+    # copy data-files into utils/ and utils/info/
     package_name = "plotter"
     data_dir = "data/"
     data_dir_info = "data/info/"
@@ -26,7 +32,7 @@ try:
     destination_info = pathlib.Path(os.getcwd() + "/plotter/utils/info")
 
     files = {f for f in im.files(package_name) if data_dir in str(f)}  # find all files
-    files_info = {f for f in im.files(package_name) if data_dir_info in str(f)}  # find all files
+    files_info = {f for f in im.files(package_name) if data_dir_info in str(f)}  # find info files
     files -= files_info
 
     for f in files:
@@ -38,7 +44,6 @@ try:
         f_destination = destination_info.joinpath(f.name)
         with open(f_destination, "wb") as d_file:
             d_file.write(f.read_binary())
-
 except OSError as _:
     print(f"{parent_dir} directory already exists.")
 
