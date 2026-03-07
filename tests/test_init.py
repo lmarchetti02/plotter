f313dc872931f85b4f3029482e79ec5fc66db45b
Luca Marchetti <90146345+lmarchetti02@users.noreply.github.com>
2026-03-07 18:33:12 +0100

    improved tests

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,16 +1,28 @@
 import os
-import pathlib
-import src.plotter
 
 
-def test_dir():
-    assert pathlib.Path("./plotter").exists()
+def test_tree(tmp_path):
+    import plotter
+
+    plotter.setup_workspace(tmp_path)
+    plotter_dir = tmp_path / "plotter"
+
+    assert plotter_dir.exists()
 
     dirs = ("img", "log", "text", "utils", "utils/info")
     for dir in dirs:
-        assert pathlib.Path("./plotter").joinpath(dir).exists()
+        assert plotter_dir.joinpath(dir).exists()
 
-
-def test_info():
-    with os.scandir(pathlib.Path("./plotter/utils/info")) as entries:
+    with os.scandir(plotter_dir / "utils/info") as entries:
         assert any(entries)
+
+    assert (plotter_dir / "log/plotter.log").exists()
+    assert (plotter_dir / "text/text_example.json").exists()
+    assert (plotter_dir / "utils/style.mplstyle").exists()
+    assert (plotter_dir / "utils/log_config.json").exists()
+
+    # repeated imports
+    del plotter
+    import plotter
+
+    plotter.setup_workspace(tmp_path)
